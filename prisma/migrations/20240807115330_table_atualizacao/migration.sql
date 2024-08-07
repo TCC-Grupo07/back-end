049505dea1c918f3be85bb26f2164f28e4bf9d91
AlvarengaEric17 <ericolivealvarenga@gmail.com>
2024-08-07 12:15:57 +0000

    migration

---
+++ prisma/migrations/20240807115330_table_atualizacao/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "sectors" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "sectors_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "suppliers" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "cnpj" TEXT NOT NULL,
+    "inscricaoEstadual" TEXT NOT NULL,
+
+    CONSTRAINT "suppliers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "leaves" (
+    "id" TEXT NOT NULL,
+    "data" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "quantidade" TEXT NOT NULL,
+
+    CONSTRAINT "leaves_pkey" PRIMARY KEY ("id")
+);
