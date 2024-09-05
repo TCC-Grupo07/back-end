d2ec4b25a3bdf98bcefe7ef9e2c9693f84118547
AlvarengaEric17 <ericolivealvarenga@gmail.com>
2024-09-05 01:43:22 +0000

    saveeeeeee

--- src/services/product/CreateProductService.ts
+++ src/services/product/CreateProductService.ts
@@ -1,3 +1,37 @@
+// import prismaClient from "../../prisma";
+
+// interface ProductRequest {
+//     name: string;
+//     price: string;
+//     description: string;
+//     banner: string;
+//     sector_id: string;
+//     quantidadeMin: string;
+// }
+
+// class CreateProductService {
+//     async execute({ name, price, description, banner, sector_id, quantidadeMin }: ProductRequest) {
+
+//         const product = await prismaClient.product.create({
+//             data: {
+//                 name: name,
+//                 price: price,
+//                 description: description,
+//                 banner: banner,
+//                 sector_id: sector_id,
+//                 quantidadeMin: quantidadeMin,
+
+
+//             }
+//         })
+
+
+//         return product
+//     }
+// }
+
+// export { CreateProductService }
+
 import prismaClient from "../../prisma";
 
 interface ProductRequest {
@@ -20,11 +54,11 @@ class CreateProductService {
                 banner: banner,
                 sector_id: sector_id,
                 quantidadeMin: quantidadeMin,
-            }
-        })
+            },
+        });
 
-        return product
+        return product;
     }
 }
 
-export { CreateProductService }
+export { CreateProductService };
