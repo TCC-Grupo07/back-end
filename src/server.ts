e7361a6cc8acc9ad4ccf9e1d538b68eb3e44ddbe
AlvarengaEric17 <ericolivealvarenga@gmail.com>
2025-02-08 11:06:32 -0300

    08-02 teste

--- src/server.ts
+++ src/server.ts
@@ -30,3 +30,188 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 
 app.listen(3333, () => console.log("SERVIDOR RODANDO ⏳"))
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express, { Request, Response, NextFunction } from "express";
+// import 'express-async-errors';
+// import cors from "cors";
+// import path from 'path'
+
+// import { router } from "./routes";
+
+// import 'dotenv/config'
+// import { MercadoPagoConfig, Payment } from 'mercadopago';
+
+// const app = express();
+// app.use(express.json())
+// app.use(cors())
+
+// app.use(router)
+
+// app.use(
+//     '/files',
+//     express.static(path.resolve(__dirname, '..', 'tmp'))
+// )
+
+
+
+
+
+
+
+
+
+
+
+
+// const client = new MercadoPagoConfig({
+//     accessToken: process.env.accessToken,
+//     options: {
+//         timeout: 5000
+//     }
+// });
+
+
+// const payment = new Payment(client);
+
+// const body = {
+//     transaction_amount: 1.10,
+//     description: 'Teste',
+//     payment_method_id: 'pix',
+//     payer: {
+//         email: 'ericolivealvarenga@gmail.com'
+//     },
+// };
+
+// payment.create({ body }).then(console.log).catch(console.log);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+//     if (err instanceof Error) {
+//         return res.status(400).json({
+//             error: err.message
+//         })
+//     }
+//     return res.status(500).json({
+//         status: 'error',
+//         message: 'Internal server erro'
+//     })
+// })
+
+// app.listen(3333, () => console.log("SERVIDOR RODANDO ⏳"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
